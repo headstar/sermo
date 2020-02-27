6c82efcf81bef2e77af789841cc3e84fefaf1f94
Per Johansson <per.johansson@post.com>
2020-02-27 21:42:51 +0000

    use Strings for system constants

--- sermo-core/src/main/java/com/headstartech/sermo/USSDSystemConstants.java
+++ sermo-core/src/main/java/com/headstartech/sermo/USSDSystemConstants.java
@@ -10,17 +10,17 @@ public class USSDSystemConstants {
 
     private USSDSystemConstants() {}
 
-    public static final Object INPUT_ITEM_DATA_KEY = new Object();
+    public static final String INPUT_ITEM_DATA_KEY = "__input_item_data__";
 
-    public static final Object INPUT_MAP_KEY = new Object();
+    public static final String INPUT_MAP_KEY = "__input_map__";
 
-    public static final Object OUTPUT_KEY = new Object();
-    public static final Object LAST_OUTPUT_KEY = new Object();
+    public static final String OUTPUT_KEY = "__output__";
+    public static final String LAST_OUTPUT_KEY = "__last_output__";
 
-    public static final Object NEXT_PAGE_KEY = new Object();
-    public static final Object PREVIOUS_PAGE_KEY = new Object();
+    public static final String NEXT_PAGE_KEY = "__next_page__";
+    public static final String PREVIOUS_PAGE_KEY = "__previous_page__";
 
-    public static final String PAGED_SCREEN_KEY = "pagedScreen";
+    public static final String PAGED_SCREEN_KEY = "__paged_screen__";
 
     public static final String MDC_MACHINE_ID_KEY = "machineId";
 
