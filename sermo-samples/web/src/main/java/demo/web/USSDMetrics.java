187892f3a2e1f365262f182d82abf4c45e25585a
Per Johansson <per.johansson@post.com>
2020-03-21 16:58:22 +0000

    more work on metrics

---
+++ sermo-samples/web/src/main/java/demo/web/USSDMetrics.java
@@ -0,0 +1,86 @@
+/*
+ *  Copyright 2020 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package demo.web;
+
+import com.headstartech.sermo.EventResult;
+import com.headstartech.sermo.MOInput;
+import com.headstartech.sermo.USSDApplication;
+import com.headstartech.sermo.USSDApplicationListener;
+import io.micrometer.core.instrument.FunctionCounter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+import io.micrometer.core.instrument.binder.MeterBinder;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @author Per Johansson
+ */
+public class USSDMetrics<S,E extends MOInput> implements MeterBinder {
+
+    private final USSDApplication<S, E> ussdApplication;
+
+    public USSDMetrics(USSDApplication<S, E> ussdApplication) {
+        this.ussdApplication = ussdApplication;
+    }
+
+    @Override
+    public void bindTo(MeterRegistry registry) {
+        MetricsListener<E> metricsListener = new MetricsListener<>(registry);
+        ussdApplication.register(metricsListener);
+
+        FunctionCounter.builder("sermo.dialog.error", metricsListener,
+                s -> s.getDialogErrorCount())
+              .register(registry);
+    }
+
+    static class MetricsListener<E extends MOInput> implements USSDApplicationListener<E> {
+
+        private AtomicLong dialogErrorCounter = new AtomicLong();
+        private final MeterRegistry meterRegistry;
+        private final ConcurrentHashMap<String, Timer.Sample> sessionTimerSamples = new ConcurrentHashMap<>();
+
+
+        public MetricsListener(MeterRegistry meterRegistry) {
+            this.meterRegistry = meterRegistry;
+        }
+
+        public long getDialogErrorCount() {
+            return dialogErrorCounter.get();
+        }
+
+        @Override
+        public void preEventHandled(String sessionId, E event) {
+            sessionTimerSamples.put(sessionId, Timer.start(meterRegistry));
+        }
+
+        @Override
+        public void postEventHandled(String sessionId, E event, EventResult eventResult) {
+            Timer.Sample sample = sessionTimerSamples.remove(sessionId);
+            sample.stop(meterRegistry.timer("sermo.dialog.event"));
+
+            if (eventResult != null) {
+                if (EventResult.ApplicationState.ERROR.equals(eventResult.getApplicationState())) {
+                    dialogErrorCounter.incrementAndGet();
+                }
+            }
+
+
+        }
+    }
+}
