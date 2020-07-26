fe58cdd78894bf3e2c7e356b10f582a20399addf
Per Johansson <per.johansson@post.com>
2020-07-26 22:44:40 +0100

    add javadoc

--- sermo-core/src/main/java/com/headstartech/sermo/statemachine/actions/ItemHandlerExitAction.java
+++ sermo-core/src/main/java/com/headstartech/sermo/statemachine/actions/ItemHandlerExitAction.java
@@ -2,7 +2,15 @@ package com.headstartech.sermo.statemachine.actions;
 
 import com.headstartech.sermo.DialogEvent;
 import org.springframework.statemachine.ExtendedState;
+import org.springframework.statemachine.action.Action;
 
+/**
+ * Exit {@link Action} assuming the object associated with a {@link MenuItem} is an instance {@link OnItemHandler}.
+ * The action calls {@link OnItemHandler#handle(ExtendedState)} 
+ * 
+ * @param <S>
+ * @param <E>
+ */
 public class ItemHandlerExitAction<S, E extends DialogEvent> extends AbstractItemObjectExitAction<S, E> {
 
     @Override
