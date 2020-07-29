16664f6eacfa26f90a1e4902a4a088f20308e0e4
Per Johansson <per.johansson@post.com>
2020-07-29 22:13:07 +0100

    add ItemHandlerExitAction

---
+++ sermo-core/src/test/java/com/headstartech/sermo/statemachine/actions/ItemHandlerExitActionTest.java
@@ -0,0 +1,54 @@
+package com.headstartech.sermo.statemachine.actions;
+
+import com.headstartech.sermo.DialogEvent;
+import com.headstartech.sermo.TestUtils;
+import com.headstartech.sermo.screen.InputMap;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.statemachine.ExtendedState;
+import org.springframework.statemachine.StateContext;
+import org.springframework.statemachine.action.Action;
+import org.springframework.statemachine.support.DefaultExtendedState;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.startsWith;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class ItemHandlerExitActionTest {
+
+    @Test
+    public void throwsExceptionIfNotOnItemHandler() {
+        // given
+        Action<String, DialogEvent> action = new ItemHandlerExitAction<>();
+
+        InputMap inputMap = InputMap.builder().
+                addMapping("1", "tr1", new Object())
+                .build();
+        StateContext<String, DialogEvent> stateContext = TestUtils.createStateContext("1", inputMap);
+
+        // when
+        Assertions.assertThrows(IllegalStateException.class,
+                () -> action.execute(stateContext));
+    }
+
+    @Test
+    public void callingItemHandler() {
+        // given
+        Action<String, DialogEvent> action = new ItemHandlerExitAction<>();
+
+        OnItemHandler onItemHandler = Mockito.mock(OnItemHandler.class);
+
+        InputMap inputMap = InputMap.builder().
+                addMapping("1", "tr1", onItemHandler)
+                .build();
+        StateContext<String, DialogEvent> stateContext = TestUtils.createStateContext("1", inputMap);
+
+        // when
+        action.execute(stateContext);
+
+        // then
+        verify(onItemHandler).handle(stateContext.getExtendedState());
+    }
+}
