c07edbe3f7e76e9a17a9b921c3a1f721b50ef7c3
Per Johansson <per.johansson@post.com>
2020-08-15 10:29:51 +0100

    add some javadoc

--- sermo-core/src/main/java/com/headstartech/sermo/states/USSDState.java
+++ sermo-core/src/main/java/com/headstartech/sermo/states/USSDState.java
@@ -5,13 +5,39 @@ import org.springframework.statemachine.action.Action;
 
 import java.util.Collection;
 
+/**
+ * Core interface representing a USSD state.
+ *
+ * @param <S>
+ * @param <E>
+ */
 public interface USSDState<S, E extends DialogEvent> {
 
+    /**
+     * Gets id of state.
+     *
+     * @return
+     */
     S getId();
 
+    /**
+     * Gets actions executed when the state is entered.
+     *
+     * @return
+     */
     Collection<Action<S, E>> getEntryActions();
 
+    /**
+     * Gets actions executed when the state is exited.
+     *
+     * @return
+     */
     Collection<Action<S, E>> getExitActions();
 
+    /**
+     * Indicates whether the state is an end state or not.
+     *
+     * @return
+     */
     boolean isEnd();
 }
